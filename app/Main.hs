b078906e04f979ee83059c23035354d8efdbced9
Ivan Dustin B. Bilon <ivan22.dust@gmail.com>
2022-04-30 00:09:11 +0800

    Working

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,18 @@
 module Main where
 
+import Data.Aeson.Encode.Pretty
+import Data.ByteString.Lazy
+import System.Environment
+import Data.Aeson
+import Parser
+import Types
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    args  <- getArgs
+    value <- parseFile (args !! 0)
+    ast   <- case value of
+        Left err  -> undefined
+        Right ast -> return ast
+    Data.ByteString.Lazy.writeFile "seeds.json" (encodePretty ast)
+    return ()
--- app/Types.hs
+++ app/Types.hs
@@ -1,6 +1,16 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 module Types where
 
-data Reference = Reference { book :: String, chapter :: Int, verse :: Int, until :: Int, is_range :: Bool } deriving Show
-data Paragraph = Paragraph { paragraph :: String } deriving Show
-data Seed      = Seed { reference :: Reference, paragraphs :: [Paragraph] } deriving Show
-data AST       = AST { seeds :: [Seed] } deriving Show
+import GHC.Generics
+import Data.Aeson
+
+data Reference = Reference { book :: String, chapter :: Int, verse :: Int, until :: Int, is_range :: Bool } deriving (Generic, Show)
+data Paragraph = Paragraph { paragraph :: String } deriving (Generic, Show)
+data Seed      = Seed { reference :: Reference, paragraphs :: [Paragraph] } deriving (Generic, Show)
+data AST       = AST { seeds :: [Seed] } deriving (Generic, Show)
+
+instance ToJSON Reference
+instance ToJSON Paragraph
+instance ToJSON Seed
+instance ToJSON AST
